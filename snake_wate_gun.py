136e354e7f02e01ae8cc10ddfcd97e66d68cb7b2
Krishna Gupta <84502036+krishnagupta1543@users.noreply.github.com>
2021-09-10 18:43:12 +0530

    Update snake_wate_gun.py

--- snake_wate_gun.py
+++ snake_wate_gun.py
@@ -2,17 +2,24 @@
 MY FIRST MINI PROJECT
 
 """
-
 import random
 print("Welcome to SNAKE-WATER-GUN Game zone")
+print("RULES TO PLAY GAME")
+print("1 - If your choice is snake and opponent choice is water than you win the round")
+print("2 - If your choice is water and opponent choice is snake than you lose the round")
+print("3 - If your choice is gun and opponent choice is water than you lose the round")
+print("4 - If your choice is water and opponent choice is gun than you win the round")
+print("5 - If your choice is gun and opponent choice is snake than you win the round")
+print("6 - If your choice is snake and opponent choice is gun than you lose the round")
+print("7 - If your choice and opponent choice is same that draw round")
 print("Enter s for ---> SNAKE")
 print("Enter w for ---> WATER")
 print("Enter g for ---> GUN")
 try:
     n = int(input("Enter the number of rounds you want to play:"))
     i = 1
-    won = 0
-    losse = 0
+    win = 0
+    lose = 0
     t = ['s', 'g', 'w']
     while(i <= n):
         print("ROUND", i)
@@ -22,56 +29,73 @@ try:
           print("Invalide input try again")
           continue
         if (c == 'g' and p == 'w'):
-          print("You Losse Round", i)
+          print("You Lose Round", i)
           print("Your choice", c)
           print("Opponent choice", p)
-          losse+=1
+          lose+=1
+          print("Your score", win)
+          print("Opponent score", lose)
       
         elif (c == 'w' and p == 'g'):
           print("Congratulation You Won Round", i)
           print("Your choice", c)
           print("Opponent choice", p)
-          won+=1
+          win+=1
+          print("Your score", win)
+          print("Opponent score", lose)
+      
       
         elif (c == 'g' and p == 's'):
           print("congratulation You Won Round", i)
           print("Your choice", c)
           print("Opponent choice", p)
-          won += 1
+          win += 1
+          print("Your score", win)
+          print("Opponent score", lose)
       
         elif( c == 's' and p == 'g'):
-          print("You Losse Round", i)
+          print("You Lose Round", i)
           print("Your choice", c)
           print("Opponent choice", p)
-          losse +=1
+          lose +=1
+          print("Your score", win)
+          print("Opponent score", lose)
       
         elif (c == 's' and p == 'w'):
           print("congratulation You Won Round", i)
           print("Your choice", c)
           print("Opponent choice", p)
-          won += 1
+          win += 1
+          print("Your score", win)
+          print("Opponent score", lose)
       
         elif(c == 'w' and p == 's'):
-          print("You Losse Round", i)
+          print("You Lose Round", i)
           print("Your choice", c)
           print("Opponent choice", p)
-          losse += 1
+          lose += 1
+          print("Your score", win)
+          print("Opponent score", lose)
       
         elif (c == p):
           print("Match Draw")
+          print("Your choice", c)
+          print("Opponent choice", p)
       
         i+=1
     
-    if (won > losse):
+    if (win > lose):
       print("******You win the match******")
       print("YOUR SCORE IS", won)
   
-    elif(won == losse):
+    elif(win == lose):
       print("******MATCH DRAW******")
+      print("Number of rounds you win", win)
+      print("Number of rounds you loss", lose)
 
     else:
       print("******YOU LOSS THE MATCH******")
-      print("YOUR SCORE IS", won)
+      print("YOUR SCORE IS", win)
 
     
 except :
