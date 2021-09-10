95c2b60e6b3bdb8d237b89668523417276f8aaa8
Krishna Gupta <84502036+krishnagupta1543@users.noreply.github.com>
2021-09-10 11:40:24 +0530

    Update snake_wate_gun.py

--- snake_wate_gun.py
+++ snake_wate_gun.py
@@ -1,54 +1,79 @@
+"""
+MY FIRST MINI PROJECT
+
+"""
+
 import random
 print("Welcome to SNAKE-WATER-GUN Game zone")
-n = int(input("Enter the number of rounds"))
-i = 1
-won = 0
-losse = 0
-p = ['s', 'g', 'w']
-while(i <= n):
-    print("ROUND", i)
-    c = input("Enter your choice: ")
-    if(c!='g' and c!='s' and c != 'w'):
-      print("Invalide input try again")
-      continue
-    p = random.choice(p)
-    if (c == 'g' and p == 'w'):
-      print("Cong...... You Won")
-      print("Your choice", c)
-      print("Opponent choice", p)
-      won+=1
-    elif (c == 'w' and p == 'g'):
-      print("You Losse")
-      print("Your choice", c)
-      print("Opponent choice", p)
-      losse += 1
-    elif (c == 'w' and p == 'g'):
-      print("Cong...... You Won")
-      print("Your choice", c)
-      print("Opponent choice", p)
-      w+=1
-    elif (c == 'g' and p == 's'):
-      print("congratulation You Won")
-      print("Your choice", c)
-      print("Opponent choice", p)
-      losse += 1
-    elif (c == 's' and p == 'w'):
-      print("congratulation You Won")
-      print("Your choice", c)
-      print("Opponent choice", p)
+print("Enter s for ---> SNAKE")
+print("Enter w for ---> WATER")
+print("Enter g for ---> GUN")
+try:
+    n = int(input("Enter the number of rounds you want to play:"))
+    i = 1
+    won = 0
+    losse = 0
+    t = ['s', 'g', 'w']
+    while(i <= n):
+        print("ROUND", i)
+        p = random.choice(t)
+        c = input("Enter your choice: ")
+        if(c!='g' and c!='s' and c != 'w'):
+          print("Invalide input try again")
+          continue
+        if (c == 'g' and p == 'w'):
+          print("You Losse Round", i)
+          print("Your choice", c)
+          print("Opponent choice", p)
+          losse+=1
+      
+        elif (c == 'w' and p == 'g'):
+          print("Congratulation You Won Round", i)
+          print("Your choice", c)
+          print("Opponent choice", p)
+          won+=1
+      
+        elif (c == 'g' and p == 's'):
+          print("congratulation You Won Round", i)
+          print("Your choice", c)
+          print("Opponent choice", p)
+          won += 1
+      
+        elif( c == 's' and p == 'g'):
+          print("You Losse Round", i)
+          print("Your choice", c)
+          print("Opponent choice", p)
+          losse +=1
+      
+        elif (c == 's' and p == 'w'):
+          print("congratulation You Won Round", i)
+          print("Your choice", c)
+          print("Opponent choice", p)
+          won += 1
+      
+        elif(c == 'w' and p == 's'):
+          print("You Losse Round", i)
+          print("Your choice", c)
+          print("Opponent choice", p)
+          losse += 1
+      
+        elif (c == p):
+          print("Match Draw")
+      
+        i+=1
+    
+    if (won > losse):
+      print("******You win the match******")
+      print("YOUR SCORE IS", won)
+  
+    elif(won == losse):
+      print("******MATCH DRAW******")
 
-    elif (c == 's' and p == 'g'):
-      print("You Losse")
-      print("Your choice", c)
-      print("Opponent choice", p)
-    elif (c == p):
-      print("Match Draw")
-    i+=1
-if (won > losse):
-  print("You win the match")
-  print("Your score is", won)
-elif(won == losse):
-  print("Match draw")
-else:
-  print("You Losse")
-  print("Your score is", won)
+    else:
+      print("******YOU LOSS THE MATCH******")
+      print("YOUR SCORE IS", won)
+
+    
+except :
+    print("Sorry please Enter Integer value of rounds you want to play")
+    
